5fbcac994f80b482c94fdd1011c16fe1d36be7c1
dentagard <dentagard@wp.pl>
2020-02-21 13:10:36 +0100

    Owl from remote server

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
     </main><!-- /.container -->
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>    
-    <script src="<?php bloginfo('template_url'); ?>/owl/dist/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 </body>
